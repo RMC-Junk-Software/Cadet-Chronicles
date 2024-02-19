fd7734cf774c4314ff4e455885741b278aa86cbe
Rishi Toranagal <toranagalrishi@gmail.com>
2024-02-18 22:19:58 -0500

    level 3 design complete and started working on level 4

---
+++ Levels/Tilesets/flag.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="rmc flag" tilewidth="84" tileheight="60" tilecount="8" columns="2">
+ <image source="../../Graphics/Textures/RMC_Flag.png" width="168" height="240"/>
+</tileset>
