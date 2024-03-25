8a94b3a56a573c1a65a27df6fe38059e87a2d3b4
joshuakalhous <156468278+joshuakalhous@users.noreply.github.com>
2024-03-24 20:22:17 -0400

    Added new tiles to level 1

---
+++ Levels/Tilesets/Lvl1BackgroundTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Lvl1BackgroundTiles" tilewidth="84" tileheight="60" tilecount="54" columns="9">
+ <image source="../../Graphics/Textures/Level1_tiles/Lvl1BackgroundTiles.png" width="756" height="360"/>
+</tileset>
